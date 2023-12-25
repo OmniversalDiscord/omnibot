cbd45e77f2ca3485165ca6d472c63e5f3996361f
fluxehub <53355311+fluxehub@users.noreply.github.com>
2023-12-25 16:33:39 +0000

    Added message if user tries to kill OmniBot

--- src/handlers/commands/kill.ts
+++ src/handlers/commands/kill.ts
@@ -14,6 +14,15 @@ export const register = (): CommandBuilder =>
 
 export default async function kill({ interaction }: Context) {
   const user = interaction.options.getUser("user", true);
+
+  if (user.id == interaction.client.user.id) {
+    const boarEmoji = interaction.client.emojis.cache.get(
+      "1009062092281745461",
+    );
+
+    return interaction.reply(`${boarEmoji ?? "🐗"}`);
+  }
+
   await interaction.reply(
     `${interaction.user} has been charged with the attempted murder of ${user} and is awaiting sentencing.`,
   );
