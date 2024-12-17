ae53e7efc9ab4f7d085c3a5d542cf3a81c90ea49
fluxehub <53355311+fluxehub@users.noreply.github.com>
2024-12-17 16:27:43 +0000

    Simplify hikari datasource and flyway

--- omnibot-infrastructure/build.gradle.kts
+++ omnibot-infrastructure/build.gradle.kts
@@ -21,9 +21,7 @@ repositories {
 
 dependencies {
     implementation(project(":omnibot-domain"))
-
-    implementation("org.springframework.boot:spring-boot-starter:3.4.0")
-    implementation("org.springframework.data:spring-data-commons:3.4.0")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.4.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
     implementation("net.dv8tion:JDA:5.2.1")
     implementation("io.github.freya022:BotCommands:ce6c5998bc")
--- omnibot-infrastructure/src/main/kotlin/co/omniversal/omnibot/infrastructure/config/DataConfig.kt
+++ omnibot-infrastructure/src/main/kotlin/co/omniversal/omnibot/infrastructure/config/DataConfig.kt
@@ -1,34 +1,15 @@
 package co.omniversal.omnibot.infrastructure.config
 
-import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import io.github.freya022.botcommands.api.core.db.HikariSourceSupplier
-import org.flywaydb.core.Flyway
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import kotlin.time.Duration.Companion.seconds
 
 @Configuration
 class DataConfig {
     @Bean("bcSourceSupplier")
-    fun databaseSource(): HikariSourceSupplier {
-        val supplier = object : HikariSourceSupplier {
-            override val source = HikariDataSource(HikariConfig().apply {
-                jdbcUrl = "jdbc:h2:mem:omnibot;MODE=PostgreSQL;DATABASE_TO_LOWER=TRUE;DEFAULT_NULL_ORDERING=HIGH"
-                maximumPoolSize = 2
-                leakDetectionThreshold = 10.seconds.inWholeMilliseconds
-            })
+    fun databaseSource(dataSource: HikariDataSource) =
+        object : HikariSourceSupplier {
+            override val source = dataSource
         }
-
-        Flyway.configure()
-            .dataSource(supplier.source)
-            .schemas("bc")
-            .locations("bc_database_scripts")
-            .validateMigrationNaming(true)
-            .loggers("slf4j")
-            .load()
-            .migrate()
-
-        return supplier
-    }
 }
