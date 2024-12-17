4db27f4eef9c9141fa62e8099f469eb14e647773
fluxehub <53355311+fluxehub@users.noreply.github.com>
2024-12-17 16:35:08 +0000

    Resize command refactor (ty freya022!)

--- omnibot-bot/src/main/kotlin/co/omniversal/omnibot/bot/modules/resize/commands/ResizeCommand.kt
+++ omnibot-bot/src/main/kotlin/co/omniversal/omnibot/bot/modules/resize/commands/ResizeCommand.kt
@@ -4,63 +4,43 @@ import co.omniversal.omnibot.bot.extensions.replyError
 import co.omniversal.omnibot.bot.filters.UserInChannelFilter
 import co.omniversal.omnibot.bot.modules.resize.config.ResizeConfig
 import dev.minn.jda.ktx.coroutines.await
+import dev.minn.jda.ktx.generics.getChannel
 import dev.minn.jda.ktx.messages.Embed
-import io.github.freya022.botcommands.api.commands.CommandPath
 import io.github.freya022.botcommands.api.commands.annotations.Command
-import io.github.freya022.botcommands.api.commands.annotations.Filter
-import io.github.freya022.botcommands.api.commands.application.ApplicationCommand
-import io.github.freya022.botcommands.api.commands.application.ApplicationCommandRejectionHandler
+import io.github.freya022.botcommands.api.commands.application.CommandScope
+import io.github.freya022.botcommands.api.commands.application.ValueRange
+import io.github.freya022.botcommands.api.commands.application.builder.filter
+import io.github.freya022.botcommands.api.commands.application.provider.GuildApplicationCommandManager
+import io.github.freya022.botcommands.api.commands.application.provider.GuildApplicationCommandProvider
 import io.github.freya022.botcommands.api.commands.application.slash.GuildSlashEvent
-import io.github.freya022.botcommands.api.commands.application.slash.annotations.JDASlashCommand
 import io.github.freya022.botcommands.api.commands.application.slash.annotations.LongRange
 import io.github.freya022.botcommands.api.commands.application.slash.annotations.SlashOption
+import io.github.freya022.botcommands.api.core.utils.awaitUnit
+import io.github.freya022.botcommands.api.core.warnNull
 import io.github.oshai.kotlinlogging.KotlinLogging
 import net.dv8tion.jda.api.entities.Guild
+import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel
 import net.dv8tion.jda.api.interactions.commands.Command.Choice
 import java.time.Instant
 
 private val logger = KotlinLogging.logger { }
 
+// With help from @freya022
 @Command
 class ResizeCommand(
     private val config: ResizeConfig,
-    private val rejectionHandler: ApplicationCommandRejectionHandler<*>
-) : ApplicationCommand() {
-    override fun getOptionChoices(guild: Guild?, commandPath: CommandPath, optionName: String): List<Choice> {
-        if (guild == null || commandPath.name != "resize" || optionName != "channel") {
-            return super.getOptionChoices(guild, commandPath, optionName)
-        }
-
-        val channels = config.channels.mapNotNull { channelId ->
-            val channel = guild.channels.find { it.id == channelId }
-            if (channel == null) {
-                logger.warn { "Could not find channel with id $channelId" }
-                return@mapNotNull null
-            } else {
-                return@mapNotNull Choice(channel.name, channel.id)
-            }
-        }
-
-        logger.info { "Found resizable channels: ${channels.map { it.name }}" }
-
-        return channels
-    }
-
-    @JDASlashCommand(name = "resize", description = "Resize a channel")
-    @Filter(UserInChannelFilter::class)
+) : GuildApplicationCommandProvider {
     suspend fun resizeCommand(
         event: GuildSlashEvent,
         @SlashOption(name = "channel", description = "The channel to resize") channelId: String,
         @SlashOption(description = "The new size of the channel") @LongRange(from = 1, to = 99) size: Int
     ) {
-        rejectionHandler
         val channel = event.guild.getVoiceChannelById(channelId)
             ?: throw IllegalArgumentException("Could not find channel with id $channelId")
 
         if (channel.members.size > size) {
-            event.replyError("You cannot resize ${channel.name} to be smaller than the current number of members")
-                .await()
-            return
+            return event.replyError("You cannot resize ${channel.name} to be smaller than the current number of members")
+                .awaitUnit()
         }
 
         channel.manager.setUserLimit(size).await()
@@ -72,4 +52,34 @@ class ResizeCommand(
             timestamp = Instant.now()
         }).await()
     }
+
+    override fun declareGuildApplicationCommands(manager: GuildApplicationCommandManager) {
+        manager.slashCommand("resize", scope = CommandScope.GUILD, function = ::resizeCommand) {
+            description = "Resize a channel"
+
+            filters += filter<UserInChannelFilter>()
+
+            option("channelId", optionName = "channel") {
+                description = "The channel to resize"
+
+                val channelChoices = getResizableChannelChoices(manager.guild)
+                logger.info { "Found resizable channels: ${channelChoices.map { it.name }}" }
+                choices = channelChoices
+            }
+
+            option("size") {
+                description = "The new size of the channel"
+
+                valueRange = ValueRange.ofLong(1, 99)
+            }
+        }
+    }
+
+    private fun getResizableChannelChoices(guild: Guild) =
+        config.channels.mapNotNull { channelId ->
+            val channel = guild.getChannel<VoiceChannel>(channelId)
+                ?: return@mapNotNull logger.warnNull { "Could not find channel with id $channelId" }
+
+            Choice(channel.name, channel.id)
+        }
 }
